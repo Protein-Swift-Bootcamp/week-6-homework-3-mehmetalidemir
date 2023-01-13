310c8741982ba966cf3472b5f5d4a315fdc76e9f
Mehmet Ali Demir <64283995+mehmetalidemir@users.noreply.github.com>
2023-01-13 17:47:04 +0300

    Fetched Images
    Images fetched from api are rendered in the Collection View

--- PinterestLayoutApp/PinterestLayoutApp/ViewController.swift
+++ PinterestLayoutApp/PinterestLayoutApp/ViewController.swift
@@ -9,12 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let photos = [1, 2, 3, 4, 5, 6, 7]
+    var heroes = [Hero]()
 
     @IBOutlet weak var collectionView: UICollectionView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let url = URL(string: "https://api.opendota.com/api/heroStats")
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+
+            if error == nil {
+                do {
+                    self.heroes = try JSONDecoder().decode([Hero].self, from: data!)
+                } catch {
+                    print("Parse Error")
+                }
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            }
+        }.resume()
+
 
         collectionView.dataSource = self
         collectionView.showsVerticalScrollIndicator = false
@@ -27,14 +43,18 @@ class ViewController: UIViewController {
 extension ViewController: UICollectionViewDataSource, PinterestLayoutDelegate {
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.count
+        return heroes.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as? CustomCollectionViewCell {
 
-            cell.imageView.image = UIImage(named: "\(photos[indexPath.row]).png")
+            let defaultLink = "https://api.opendota.com"
+            let completeLink = defaultLink + heroes[indexPath.row].img
+            
+
+            cell.imageView.downloaded(from: completeLink)
             cell.clipsToBounds = true
             cell.layer.cornerRadius = 15
             cell.imageView.contentMode = .scaleAspectFill
@@ -46,11 +66,47 @@ extension ViewController: UICollectionViewDataSource, PinterestLayoutDelegate {
 
     func collectionView(_ collectionView: UICollectionView, heightForPhotoAtIndexPath indexPath: IndexPath) -> CGFloat {
 
-        let image = UIImage(named: "\(photos[indexPath.row]).png")
+        let defaultLink = "https://api.opendota.com"
+        let completeLink = defaultLink + heroes[indexPath.row].img
+        let urlImage = URL(string: completeLink)
+
+        let data = try? Data(contentsOf: urlImage!)
+
+        let image = UIImage(data: data!)
 
         if let height = image?.size.height {
-            return height / 4
+            return height
         }
         return 0.0
     }
 }
+
+
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { [weak self] in
+                self?.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
+
+struct Hero: Decodable {
+    let localized_name: String
+    let img: String
+    
+}
+
